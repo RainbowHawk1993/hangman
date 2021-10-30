1af97bbdb75061928451d5527977d6a2efe11efc
Nick <nicklos550@gmail.com>
2021-10-30 20:32:38 +0300

    Initial

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("+---+\n|   |n|\n|\n|\n|\n=========")          //0
+	fmt.Println("+---+\n|   |\n|   O\n|\n|\n|\n=========")     //
+	fmt.Println("+---+\n|   |\n|   O\n|   |\n|\n|\n=========") //
+	fmt.Println("+---+\n|   |\n|   O\n|  /|\n|\n|\n=========")
+	fmt.Println("+---+\n|   |\n|   O\n|  /|\\\n|\n|\n=========")       //
+	fmt.Println("+---+\n|   |\n|   O\n|  /|\\\n|  /\n|\n=========")    //
+	fmt.Println("+---+\n|   |\n|   O\n|  /|\\\n|  / \\\n|\n=========") //6
+
+}
