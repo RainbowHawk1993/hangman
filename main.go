b8c93643f2d85bb8200c933110955ea8a1c7febf
Nick <nicklos550@gmail.com>
2021-10-30 20:38:40 +0300

    Saved all words to array

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
+	txtFilename := flag.String("txt", "words.txt",
+		"a txt file with 1 word per line")
+	flag.Parse()
+
+	file, err := os.Open(*txtFilename)
+	if err != nil {
+		exit("Failed to open txt file.")
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var text []string
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
 
 	fmt.Println("+---+\n|   |n|\n|\n|\n|\n=========")          //0
 	fmt.Println("+---+\n|   |\n|   O\n|\n|\n|\n=========")     //
@@ -14,4 +34,9 @@ func main() {
 	fmt.Println("+---+\n|   |\n|   O\n|  /|\\\n|  /\n|\n=========")    //
 	fmt.Println("+---+\n|   |\n|   O\n|  /|\\\n|  / \\\n|\n=========") //6
 
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
